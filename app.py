3f4b88637957511cd686f76a9c35894ed91a7b85
lixiang <32087152+lix8886@users.noreply.github.com>
2025-07-29 14:33:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,19 +49,56 @@ st.html(
     .st-key-screen_stats {
         height: 0 !important;
     }
-    .data-testid="manage-app-button" {
-        display:none;
-    }
     ._container_gzau3_1 {
-        display: none;
+        display: none !important;
     }
     ._profileContainer_gzau3_53 {
-        display: none;
+        display: none !important;
     }
 </style>
 """
 )
 
+
+def check_port(host, port, timeout=1):
+    try:
+        socket.setdefaulttimeout(timeout)
+        with socket.create_connection((host, port)):
+            return True
+    except Exception:
+        return False
+
+
+def get_ip():
+    """获取当前主机的非回环 IP 地址"""
+    ip_list = []
+    for iface, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET and not addr.address.startswith("127."):
+                ip_list.append(addr.address)
+    return ip_list
+
+
+HOST = get_ip()[0]
+URL = f"http://{HOST}:{PORT}"
+
+if "__first_server__" not in st.session_state:
+    st.session_state["__first_server__"] = True
+
+    print(f"正在检查API服务（{URL=}）...")
+    if not check_port(HOST, PORT):
+        subprocess.Popen(
+            ["python", "server.py"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        print(f"API服务启动成功{URL=}")
+        st.toast(f"API服务启动成功{URL=}")
+    else:
+        print(f"API服务已启动{URL=}")
+        st.toast(f"API服务已启动{URL=}")
+
+
 screen_stats = ScreenData(setTimeout=10).st_screen_data()
 innerHeight = screen_stats["innerHeight"] - 10
-components.iframe('https://markdown.com.cn/editor/', height=innerHeight)
+components.iframe(URL, height=innerHeight)
